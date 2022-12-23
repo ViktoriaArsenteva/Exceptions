1c88e9356654c785ed3390cdfe21c2d2ddded6f0
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-12-23 13:49:34 +0300

    task2 solved

--- Task2.java
+++ Task2.java
@@ -1,15 +1,17 @@
 public class Task2 {
     public static void main(String[] args) {
-        
+        task2();
     }
     public static void task2() {
         try {
+            int[] intArray = new int[10];
             int d = 0;
-            double[] catchedRes1 = new double [8];
+            double catchedRes1 = intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
          } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
          }
+         
          
     }
 }
--- Task3.java
+++ Task3.java
@@ -7,15 +7,15 @@ public class Task3 {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
         }
      }
-     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+     public static void printSum(int a, int b) {
         System.out.println(a + b);
      }
      
